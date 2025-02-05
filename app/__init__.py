a5b5db2a5539aea0c83cd8d7e6c3cee7a01e13af
Felipe Zanardo <fzs.fto@hotmail.com>
2025-02-05 15:53:31 -0300

    modficicações de implementação db + configurações necessarias preprod

--- app/__init__.py
+++ app/__init__.py
@@ -5,17 +5,10 @@ from .models import db
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "your_secret_key"
-    app.config["SQLALCHEMY_DATABASE_URI"] = (
-        "postgresql://username:password@localhost/dbname"
-    )
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config.from_object("config.Config")
 
     db.init_app(app)
 
     app.register_blueprint(views)
-
-    with app.app_context():
-        db.create_all()
 
     return app
---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+
+class Config:
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
